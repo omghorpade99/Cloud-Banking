0c7c0a52687d4c055a97266278987b7052398e64
omghorpade99 <112423457+omghorpade99@users.noreply.github.com>
2024-09-14 13:40:45 +0530

    Update bnkms.sql

--- DATABASE FILE/bnkms.sql
+++ DATABASE FILE/bnkms.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 27, 2021 at 12:24 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
